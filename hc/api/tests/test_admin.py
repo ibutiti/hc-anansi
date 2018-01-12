c37b7461cb1f15d71f00fdf8ad7557c5b33f5bc6
Allan Ibutiti <allan@Allans-MacBook-Pro-2.local>
2018-01-12 12:04:05 +0300

    Fixed api/tests/test_admin.py tests

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -7,8 +7,10 @@ class ApiAdminTestCase(BaseTestCase):
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
-
-        ### Set Alice to be staff and superuser and save her :)
+        self.check.user.is_superuser = True
+        self.check.user.is_staff = True
+        self.check.user.save()
+        # Set Alice to be staff and superuser and save her :)
 
     def test_it_shows_channel_list_with_pushbullet(self):
         self.client.login(username="alice@example.org", password="password")
@@ -16,4 +18,5 @@ class ApiAdminTestCase(BaseTestCase):
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
         ch.save()
 
-        ### Assert for the push bullet
+        self.assertEqual(ch.kind, 'pushbullet')
+        # Assert for the push bullet
